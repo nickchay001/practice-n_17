189489073eeadfdeb89893b17917228dbc994968
nickchay001 <119807798+nickchay001@users.noreply.github.com>
2022-12-15 19:15:39 +0200

    add to repository

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+import * as flsFunctions from "./modules/functions.js";
+
+flsFunctions.isWebp();
+
+const iconMenu = document.querySelector('.menu__icon');
+if(iconMenu){
+	const headerMenu = document.querySelector('.header__menu');
+	iconMenu.addEventListener("click", function(e) {
+		document.body.classList.toggle('_lock');
+		iconMenu.classList.toggle('_active');
+		headerMenu.classList.toggle('_active');
+	})
+}
